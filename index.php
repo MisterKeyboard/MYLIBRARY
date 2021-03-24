bb1e62331fdeb2d56092cbb119ca1425403291ec
MisterKeyboard <antoine.clavier@hotmail.fr>
2021-03-24 16:19:49 +0100

    css

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My library</title>
     <link rel="icon" href="img/book-shelf.png">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./CSS/style.css">
 </head>
 
 <body>
@@ -14,22 +14,32 @@
 
     <section>
 
+        <div class="welcome">
+            <h1>WELCOME IN MY LIBRARY</h1>
+            <form method="POST" action="add.php">
 
-        <h1>WELCOME IN MY LIBRARY</h1>
-        <form method="POST" action="add.php">
+                <label for="title">Book title</label>
+                <input type="text" name="title">
 
-            <label for="title">Book title</label>
-            <input type="text" name="title">
+                <label for="price">Price</label>
+                <input type="number" name="price" min="0" step="0.01">
 
-            <label for="price">Price</label>
-            <input type="number" name="price" min="0" step="0.01">
+                <label for="date">Release date</label>
+                <input type="number" name="date" step="1">
 
-            <label for="date">Release date</label>
-            <input type="number" name="date" step="1">
+                <label for="language">Language</label>
+                <input type="text" name="language">
 
-
-        </form>
+                <button type="submit">SEND</button>
+            </form>
+        </div>
     </section>
 
 </body>
+
+</html>
+
+
+
+
 
